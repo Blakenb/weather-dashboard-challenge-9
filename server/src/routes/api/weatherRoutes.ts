0433557dc4eb8f1361862f6a330d2530748dcbc9
Blakenb <blakenb87@gmail.com>
2025-02-15 16:46:47 -0600

    working on the challenge

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,28 @@
-import { Router, type Request, type Response } from 'express';
+import { Router, type Request, type Response } from "express";
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post("/", (req: Request, res: Response) => {
+  console.log(req);
+  console.log(res);
   // TODO: GET weather data from city name
+
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get("/history", async (req: Request, res: Response) => {
+  console.log(req);
+  console.log(res);
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete("/history/:id", async (req: Request, res: Response) => {
+  console.log(req);
+  console.log(res);
+});
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,22 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 // Import the routes
-import routes from './routes/index.js';
+import routes from "./routes/index.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
-
+app.use(express.static(path.join(__dirname, "../../client/dist/")));
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
